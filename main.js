b5086be0f5f09068128c0167d7af7f4806c5a382
Daniel Windsor <dw.accounts@protonmail.com>
2020-02-24 12:48:56 +1300

    Init commit

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', startGame)
+
+function startGame() {
+  console.log("started")
+
+  //Instantiate click listeners
+
+  generateBoard()
+}
+
+//Programmatically creates Chess Board
+function generateBoard() {
+
+}
