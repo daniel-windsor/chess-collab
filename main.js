587f37a3929a97ef702add2a318206fb1760d58b
Daniel Windsor <dw.accounts@protonmail.com>
2020-02-24 13:18:07 +1300

    Add board object

--- main.js
+++ main.js
@@ -11,4 +11,19 @@ function startGame() {
 //Programmatically creates Chess Board
 function generateBoard() {
 
+  let board = {}
+  board["cells"] = []
+
+  for (let i = 0; i < 64; i++) {
+    board.cells.push({})
+    board.cells[i].row = i % 8
+    board.cells[i].col = Math.floor(i/8)
+
+    if(board.cells[i].row % 2 == 0) {
+      board.cells[i].color = "black";
+    } else {
+      board.cells[i].color = "white";
+    }
+  }
+    console.table(board.cells)
 }
