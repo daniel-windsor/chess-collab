1c25541f6b0a47b9abd3ec22d17ededa6774dc79
Daniel Windsor <dw.accounts@protonmail.com>
2020-02-24 14:06:09 +1300

    Generate board

--- main.js
+++ main.js
@@ -17,13 +17,28 @@ function generateBoard() {
   for (let i = 0; i < 64; i++) {
     board.cells.push({})
     board.cells[i].row = i % 8
-    board.cells[i].col = Math.floor(i/8)
+    board.cells[i].col = Math.floor(i / 8)
 
-    if(board.cells[i].row % 2 == 0) {
-      board.cells[i].color = "black";
-    } else {
+    if ((i + board.cells[i].col) % 2 == 0) {
       board.cells[i].color = "white";
+    } else {
+      board.cells[i].color = "black";
     }
   }
-    console.table(board.cells)
+  const boardNode = document.getElementsByClassName('board')[0]
+  board.cells.reduce(cellsToNodes, boardNode)
+}
+
+function cellsToNodes(boardNode, cell) {
+  const node = document.createElement('div');
+  node.classList.add('cell')
+
+  if (cell.color == "white") {
+    node.classList.add('white')
+  } else {
+    node.classList.add('black')
+  }
+
+  boardNode.appendChild(node)
+  return boardNode;
 }
